bb2d16470f96234c940ed274d866199ce27e4d31
Bijoy Biswas <biswasbijoy414@gmail.com>
2023-10-14 01:39:47 +0600

    name edited and new path added from predict

--- myhome/urls.py
+++ myhome/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from predict import urls as predict_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('started/', views.landing_page, name='landing_page'),
+    path('', views.start, name='start'),
+    path('started/', views.auth, name='auth'),
     path('started/signup/', views.signup, name='signup'),
     path('started/login/', views.login, name='login'),
+    path('', include(predict_urls, namespace='predict')),
 ]
