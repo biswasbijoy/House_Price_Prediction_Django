30539552bd6dfc5a3aee23370d366da658d76e91
Bijoy Biswas <biswasbijoy414@gmail.com>
2023-10-18 11:45:37 +0600

    update url for predict price

--- predict/urls.py
+++ predict/urls.py
@@ -3,5 +3,7 @@ from predict.views import views
 
 app_name = 'predict'
 urlpatterns = [
-    path('predict', views.prediction, name='predict'),
+    path('prediction.html', views.prediction, name='predict'),
+    path('predict', views.prediction_result, name='prediction_result'),
+    path('ehehbouy', views.location, name='ehehbouy'),
 ]
