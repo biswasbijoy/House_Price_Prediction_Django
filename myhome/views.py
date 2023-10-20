31810a6827da266f56b0dafe12613694b8d88393
Bijoy Biswas <biswasbijoy414@gmail.com>
2023-10-21 02:06:05 +0600

    mode auth function to posts app

--- myhome/views.py
+++ myhome/views.py
@@ -5,13 +5,6 @@ def start(request):
     return render(request, 'starting.html')
 
 
-def auth(request):
-    if request.user.is_authenticated:
-        return render(request, 'home.html')
-    else:
-        return render(request, 'auth.html')
-
-
 def signup(request):
     return render(request, 'signup.html')
 
