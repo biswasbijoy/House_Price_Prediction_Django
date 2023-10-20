6615afc849adc0ccd08182792b31c94f39e0be00
Bijoy Biswas <biswasbijoy414@gmail.com>
2023-10-21 02:06:31 +0600

    resitered Post model and Category model

---
+++ posts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from posts.models.categories import PostCategory
+from posts.models.models import Post
+admin.site.register(PostCategory)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'title', 'description', 'published']
+    list_filter = ['author', 'category']
