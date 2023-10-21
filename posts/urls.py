bc4e45425bc850c0a6a41dc975f095d514678370
Bijoy Biswas <biswasbijoy414@gmail.com>
2023-10-21 19:46:24 +0600

    filter's urls added

--- posts/urls.py
+++ posts/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('new-post/', PostCreateView.as_view(), name = 'new_post'),
     path('content/update/<int:pk>', PostUpdateView.as_view(), name = 'update_post'),
     path('content/delete/<int:pk>', PostDeleteView.as_view(), name = 'delete_post'),
+    path('filter', filter_posts, name = 'filter'),
     path('home', home_auth, name = 'home'),
 ]
