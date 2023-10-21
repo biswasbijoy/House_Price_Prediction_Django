57a26513bbc5784e0831674e6032968691530400
Bijoy Biswas <biswasbijoy414@gmail.com>
2023-10-21 19:46:53 +0600

    implement filter view

--- posts/views/views.py
+++ posts/views/views.py
@@ -3,7 +3,8 @@ from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
 
-from posts.forms.forms import PostForm, UpdateForm
+from posts.forms.forms import PostForm
+from posts.models.categories import PostCategory
 from posts.models.models import Post
 
 check = False
@@ -16,11 +17,9 @@ def auth(request):
         return False
 
 
+@login_required(login_url='/login')
 def home_auth(request):
-    if auth(request):
-        return render(request, 'home.html')
-    else:
-        return render(request, 'auth.html')
+    return render(request, 'home.html')
 
 
 class BrowserView(ListView):
@@ -40,15 +39,15 @@ class PostDetailsView(DetailView):
 
 class PostCreateView(CreateView):
     model = Post
-    # form_class = PostForm
+    form_class = PostForm
     template_name = 'posts/create_post.html'
-    fields = ['title', 'author', 'description']
+    # fields = ['title', 'author', 'description', 'category']
 
 
 class PostUpdateView(UpdateView):
     model = Post
     template_name = 'posts/update_post.html'
-    fields = ['title', 'description']
+    fields = ['title', 'description', 'category']
     # form_class = UpdateForm
 
 
@@ -56,3 +55,22 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'posts/delete_post.html'
     success_url = '/browse'
+
+
+def filter_posts(request):
+    if request.method == 'GET':
+        selected_categories = request.GET.getlist('categories')
+
+        if selected_categories:
+            posts = Post.objects.filter(category__in=selected_categories)
+        else:
+            posts = Post.objects.all()
+
+        category_choices = PostCategory.CATEGORY_OPTION_CHOICES
+
+        context = {
+            'object_list': posts,
+            'category_choices': category_choices,
+        }
+
+        return render(request, 'posts/browse.html', context)
