939391e947f517859e238a2d3e7e9739d65ff655
Bijoy Biswas <biswasbijoy414@gmail.com>
2023-10-21 19:43:18 +0600

    Update PostForm

--- posts/forms/forms.py
+++ posts/forms/forms.py
@@ -1,30 +1,22 @@
 from django import forms
-from posts.models.models import Post, PostCategory
-
-from django import forms
-from posts.models.models import Post, PostCategory
-
-choice_list = PostCategory.objects.all().values_list('category', 'category')
+from posts.models.models import Post
+from posts.models.categories import PostCategory
 
 
 class PostForm(forms.ModelForm):
+    category = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        choices=PostCategory.CATEGORY_OPTION_CHOICES,
+        required=False
+    )
+
     class Meta:
         model = Post
-        fields = ('title', 'author', 'category', 'description')
+        fields = ['title', 'author', 'description', 'category']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'elder', 'type': 'visible'}),
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'author', 'type': 'hidden'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
-        }
-
-
-class UpdateForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ('title', 'description')
-
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.CheckboxSelectMultiple(attrs={'class': 'form-control'}),
         }
