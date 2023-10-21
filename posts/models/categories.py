10d1909601b1fc62b9c2fea8b5e57a06acd4302c
Bijoy Biswas <biswasbijoy414@gmail.com>
2023-10-21 19:44:13 +0600

    convert tuple to list. Not matter

--- posts/models/categories.py
+++ posts/models/categories.py
@@ -3,11 +3,14 @@ from posts.models.base_time_stamp import BaseStampStampModel
 
 
 class PostCategory(BaseStampStampModel):
-    CATEGORY_OPTION_CHOICES = (
-        ('Cat_one', 'Sell',),
-        ('Cat_two', 'Buy',),
-        ('Cat_three', 'Information',),
-    )
+    CATEGORY_OPTION_CHOICES = [
+        ('1', 'Sell',),
+        ('2', 'Buy',),
+        ('3', 'Information',),
+    ]
 
-    category = models.CharField(max_length=50, choices=CATEGORY_OPTION_CHOICES, default='Cat_one')
+    category = models.CharField(max_length=50, choices=CATEGORY_OPTION_CHOICES, default='1')
     value = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.get_category_display()
