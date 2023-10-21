6a8c441542a4ebc5ee5676fd9d1d5a649a3f00d8
Bijoy Biswas <biswasbijoy414@gmail.com>
2023-10-21 19:41:54 +0600

    removed signup and login function and moved to homeauth

--- myhome/views.py
+++ myhome/views.py
@@ -5,9 +5,3 @@ def start(request):
     return render(request, 'starting.html')
 
 
-def signup(request):
-    return render(request, 'signup.html')
-
-
-def login(request):
-    return render(request, 'login.html')
